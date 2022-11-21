1291006f508730474dae6dff94c7fe0b510c7b62
Sofia Bezertzi <sofia_bezertzi@waters.com>
2022-11-21 10:11:45 +0000

    adds hello class to dotnet project

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/Hello.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace McrDigital.Bootcamp1.Checkout
+{
+    public class Hello
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
